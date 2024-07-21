abfb05be70473a663e82fc972e6b52ccd775b7e2
Turgay-DEMIREL <turgaydemirel23@gmail.com>
2024-07-21 18:48:34 +0300

    Update Odev-27.js.td

--- Odev-27.js
+++ Odev-27.js
@@ -2,8 +2,20 @@
 1. Bir dizideki sayıları çift mi tek mi olduklarına göre ayırmak için filter metodunu kullanın.
 
 Örnek Dizi: [4, 7, 10, 15, 18, 21]
+const sayilar = [4, 7, 10, 15, 18, 21];
+const ciftSayilar = sayilar.filter(num => % 2 === 0);
+console.log(ciftSayilar);
+
+const tekSayilar = sayilar.filter(num => % 2 !== 0);
+console.log(tekSayilar);
 
 2. Bir dizideki her sayıyı mutlak değere çeviren bir map fonksiyonu yazın.
 
 Örnek Dizi: [-10, 20, -30, 40, -50, 60]
 */
+const Dizi = [-10, 20, -30, 40, -50, 60];
+
+const mutlakDeger = (sayi) => {
+    return Math.abs(sayi);
+  };
+let yeniDizi = dizi.map(mutlakDeger);
